4d34d63ee3767587d957b4998193fd4d93dc9ff3
Simone Sciarrati <s.sciarrati@gmail.com>
2021-09-13 20:02:44 +0200

    spok-1.18.2 with clusterCIDR patch

--- version.go
+++ version.go
@@ -23,7 +23,7 @@ var Version = KOPS_RELEASE_VERSION
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.18.2"
+	KOPS_RELEASE_VERSION = "Bravo-1.18.2"
 	KOPS_CI_VERSION      = "1.18.3"
 )
 
